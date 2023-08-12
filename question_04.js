3c9717b95b6b652a479ef6cbcdd9065b9b93fc36
HafsaAfzal <hafsaafzal380@gmail.com>
2023-08-12 14:07:37 +0500

    correct code of  questin 4 added

--- question_04.js
+++ question_04.js
@@ -1,3 +1,12 @@
 // Imagine you have an object called book with properties like title, author, and pages.
 // How would you define a method called printInfo that logs a message displaying the
 // book's title, author, and number of pages using this keyword?
+const book ={
+    title:"Jannat kay pattay",
+    author:"Nimra Ahmed",
+    pages:544,
+    printInfo:function(){
+        console.log(`The amazing novel ${this.title} is written by ${this.author} and this novel has ${this.pages} pages`)
+    }
+}
+book.printInfo();
