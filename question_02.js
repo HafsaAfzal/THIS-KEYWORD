0043e4de01c9b62e1526683a554e6d051bb4fa40
HafsaAfzal <hafsaafzal380@gmail.com>
2023-08-12 12:33:16 +0500

    initiall commit

---
+++ question_02.js
@@ -0,0 +1,3 @@
+// Create a constructor function called `Car` that has `brand` and `speed` properties.
+// Add a method called `accelerate` that increases the speed of the car by a given
+// value. Add a method called `print Speed` that logs the current speed of the car.
