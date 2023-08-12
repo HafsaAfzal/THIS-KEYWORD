db28840c42aa57be84fa3f612e244f4819021013
HafsaAfzal <hafsaafzal380@gmail.com>
2023-08-12 21:53:25 +0500

    initiall commit

---
+++ question_06.js
@@ -0,0 +1,3 @@
+// Consider an object called counter with a property count and two methods:
+// increment and reset. How would you define the increment method so that it
+// increases the count property by 1 using this keyword?
