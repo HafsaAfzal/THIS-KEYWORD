a30fbb10c804203022bc6dbfc19ed9ae6495d01f
HafsaAfzal <hafsaafzal380@gmail.com>
2023-08-12 13:52:31 +0500

    initiall commit

---
+++ question_04.js
@@ -0,0 +1,3 @@
+// Imagine you have an object called book with properties like title, author, and pages.
+// How would you define a method called printInfo that logs a message displaying the
+// book's title, author, and number of pages using this keyword?
