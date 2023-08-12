042ae40c9e2b9b7a76964fe907245735b1d22f0b
HafsaAfzal <hafsaafzal380@gmail.com>
2023-08-12 21:51:40 +0500

    correct code of question 5 added

--- question_05.js
+++ question_05.js
@@ -1,12 +1,12 @@
 // You have a constructor function called Person that creates person objects with
 // properties like name and age. How would you define a method called introduce that
 // logs a greeting message including the person's name and age, using this keyword?
-function person(naam,age){
+function Person(naam,age){
     this.naam=naam,
     this.age=age,
     this.intro=function(){
-        console.log(`hello ${this.naam},your age is ${this.age}`)
+        console.log(`Hello! ${this.naam},your age is ${this.age}`)
     }
-}
-let person1=new person("Hafsa",20);
+}   
+let person1=new Person("Hafsa",20);
 person1.intro();
