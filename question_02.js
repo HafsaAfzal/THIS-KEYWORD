7c680aeda5373537434a9aeb40445dda8735b257
HafsaAfzal <hafsaafzal380@gmail.com>
2023-08-12 13:17:33 +0500

    make some changes

--- question_02.js
+++ question_02.js
@@ -1,16 +1,17 @@
 // Create a constructor function called `Car` that has `brand` and `speed` properties.
 // Add a method called `accelerate` that increases the speed of the car by a given
 // value. Add a method called `print Speed` that logs the current speed of the car.
-function car(){
-    brand="Honda",
-    newSpeed=0
-    function accelerate(speed){
+function car(brand){
+    this.brand=brand,
+    this.newSpeed=0;
+    this.accelerate=function (speed){
         newSpeed=newSpeed+speed;
        return newSpeed
     }
-    function printSpeed(newSpeed){
+    this.printSpeed=function (newSpeed){
         console.log(newSpeed);
     }
 }
-let car1=car();
+let car1=new car("Honda");
 car1.accelerate(50);
+car1.printSpeed();
