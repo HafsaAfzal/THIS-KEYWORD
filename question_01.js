08de674b6755e07d8d0dc6c432dcc4b8a90ff3ee
HafsaAfzal <hafsaafzal380@gmail.com>
2023-08-12 12:27:38 +0500

    code added

---
+++ question_01.js
@@ -0,0 +1,8 @@
+// Create an object called `person` with a `name` property and a `greet` method. The
+// `greet` method should log a greeting message with the person's name.
+const person ={
+    name:"Hafsa",
+    greet:function(){
+        console.log(`Hello! ${person.name}`)
+    }
+}
