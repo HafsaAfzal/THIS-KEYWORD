0e39a053cc062f357f8306211d99ec46b243235a
HafsaAfzal <hafsaafzal380@gmail.com>
2023-08-12 22:42:29 +0500

    code of question 8

---
+++ question_08.js
@@ -0,0 +1,10 @@
+// Imagine you have a function called play which is defined inside an object called
+// game. How would you call the play function from within the object using this
+// keyword?
+const obj={
+    nam:"Hafsa",
+    play:function(){
+        console.log(`Are you ready ${this.nam} to play`);
+    }
+}
+obj.this.play();
